e1df60e875a616f81224e85732a07a1e516c3e08
JeromeNL <jw.kwetters@student.avans.nl>
2023-01-28 19:57:50 +0100

    Initial Commit

---
+++ SwiftMapApp/SwiftMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMapAppApp.swift
+//  SwiftMapApp
+//
+//  Created by Joram Kwetters on 28/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
