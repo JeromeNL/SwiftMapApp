2b34514b68dc51b5d01a7ed6169abe68d90c1488
JeromeNL <jw.kwetters@student.avans.nl>
2023-01-29 00:07:16 +0100

    initial commit - MapApp Setup Tutorial #1

---
+++ SwiftMapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  SwiftMapApp
+//
+//  Created by Joram Kwetters on 28/01/2023.
+//
+
+import Foundation
+import MapKit
+
+struct Location{
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
