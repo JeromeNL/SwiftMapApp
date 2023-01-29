faa8b69e83a76616d4f40a7e6ff808390a9a9211
JeromeNL <jw.kwetters@student.avans.nl>
2023-01-29 12:07:22 +0100

    Add Map Tutorial #3

--- SwiftMapApp/Views/LocationsView.swift
+++ SwiftMapApp/Views/LocationsView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import MapKit
 
 
 
@@ -14,12 +15,14 @@ struct LocationsView: View {
     
     //@StateObject private var vm = LocationsViewModel()
     @EnvironmentObject private var vm: LocationsViewModel
+//    @State private var mapRegion: MKCoordinateRegion = MKCoordinateRegion(
+//        center: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922),
+//        span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
     
     var body: some View {
-        List{
-            ForEach(vm.locations){
-                Text($0.name)
-            }
+        ZStack{
+            Map(coordinateRegion: $vm.mapRegion)
+            .ignoresSafeArea()
         }
     }
 }
