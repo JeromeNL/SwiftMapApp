633443a690aa5d1e10879c7dc58e810e19cde7d1
JeromeNL <jw.kwetters@student.avans.nl>
2023-01-29 14:13:26 +0100

    Using a List as a custom animated menu Tutorial #4

--- SwiftMapApp/Views/LocationsView.swift
+++ SwiftMapApp/Views/LocationsView.swift
@@ -13,19 +13,24 @@ import MapKit
 
 struct LocationsView: View {
     
-    //@StateObject private var vm = LocationsViewModel()
     @EnvironmentObject private var vm: LocationsViewModel
-//    @State private var mapRegion: MKCoordinateRegion = MKCoordinateRegion(
-//        center: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922),
-//        span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+    
     
     var body: some View {
         ZStack{
             Map(coordinateRegion: $vm.mapRegion)
-            .ignoresSafeArea()
+                .ignoresSafeArea()
+            
+            VStack(spacing: 0){
+                header
+                .padding()
+                
+                Spacer()
+            }
+              
+            }
         }
     }
-}
 
 struct LocationsView_Previews: PreviewProvider {
     static var previews: some View {
@@ -33,3 +38,37 @@ struct LocationsView_Previews: PreviewProvider {
             .environmentObject(LocationsViewModel())
     }
 }
+
+extension LocationsView{
+    
+    private var header: some View{
+        VStack{
+            Button(action: vm.toggleLocationsList){
+                Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
+                    .font(.title2)
+                    .fontWeight(.black)
+                    .foregroundColor(.primary)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .animation(.none, value: vm.mapLocation)
+                    //.background(Color.red)
+                    .overlay(alignment: .leading){
+                        Image(systemName: "arrow.down")
+                            .foregroundColor(.primary)
+                            .padding()
+                            .rotationEffect(Angle(degrees:
+                                vm.showLocationsList ? 180 : 0))
+                    }
+            }
+                   
+            if(vm.showLocationsList){
+                LocationsListView()
+            }
+           
+        }
+        .background(.thickMaterial)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.3),
+                radius: 20, x: 0, y: 15)
+    }
+} 
